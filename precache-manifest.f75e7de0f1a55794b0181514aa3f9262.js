0bb6f32ae09336000caec8be7a6f2b861f59cd9a
zzzonki <zzzonki@gmail.com>
2020-10-12 17:10:09 +0300

    Updates

---
+++ precache-manifest.f75e7de0f1a55794b0181514aa3f9262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6a2ba52c059670cfb0ee0323b55f15",
+    "url": "/react_gallery_app/index.html"
+  },
+  {
+    "revision": "65ddad75dbdf416a73c9",
+    "url": "/react_gallery_app/static/css/main.1f8ee617.chunk.css"
+  },
+  {
+    "revision": "e5e561d50703ec47bd3f",
+    "url": "/react_gallery_app/static/js/2.d0c90b0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_gallery_app/static/js/2.d0c90b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ddad75dbdf416a73c9",
+    "url": "/react_gallery_app/static/js/main.dc65109e.chunk.js"
+  },
+  {
+    "revision": "6e00019e7ef884e02719",
+    "url": "/react_gallery_app/static/js/runtime-main.4697ff09.js"
+  }
+]);
